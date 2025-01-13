6a90718356cfd54ac66b1596ab3035b9f4a7eec8
Flurin Brühwiler <flurin.bruehwiler@cmiag.ch>
2025-01-13 16:36:18 +0100

    add widget and relative path to lsp server

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/reqnrollriderplugin/BddLspServerSupportProvider.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/reqnrollriderplugin/BddLspServerSupportProvider.kt
@@ -1,10 +1,13 @@
 package com.jetbrains.rider.plugins.reqnrollriderplugin
 
 import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.lsp.api.LspServer
 import com.intellij.platform.lsp.api.LspServerSupportProvider
 import com.intellij.platform.lsp.api.ProjectWideLspServerDescriptor
+import com.intellij.platform.lsp.api.lsWidget.LspServerWidgetItem
 
 class BddLspServerSupportProvider : LspServerSupportProvider {
     override fun fileOpened(
@@ -17,17 +20,18 @@ class BddLspServerSupportProvider : LspServerSupportProvider {
         }
     }
 
-//    override fun createLspServerWidgetItem(
-//        lspServer: LspServer,
-//        currentFile: VirtualFile?
-//    ) =
-//        LspServerWidgetItem(
-//            lspServer, currentFile,
-//            FooIcons.PluginIcon, FooConfigurable::class.java
-//        )
+    override fun createLspServerWidgetItem(
+        lspServer: LspServer,
+        currentFile: VirtualFile?
+    ) =
+        LspServerWidgetItem(
+            lspServer,
+            currentFile,
+            icon = ReqnrollIcons.Icons.ReqnrollLogo
+        )
 }
 
 private class BddLspServerDescriptor(project: Project) : ProjectWideLspServerDescriptor(project, "Foo") {
     override fun isSupportedFile(file: VirtualFile) = file.extension == "feature"
-    override fun createCommandLine() = GeneralCommandLine("C:\\CMI-GitHub\\Tools\\BddLsp\\BddLspServer\\BddLspServer\\bin\\Debug\\net9.0\\BddLspServer.exe")
+    override fun createCommandLine() = GeneralCommandLine(PathManager.getPluginsPath() + "/reqnrollriderplugin/lsp/BddLspServer.exe")
 }
